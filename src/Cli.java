a46f943a9ad53f80f33655be76af31f15283eb07
Alex <amacielcastanho@gmail.com>
2021-04-28 14:09:00 -0300

    Add files via upload

---
+++ src/Cli.java
@@ -0,0 +1,59 @@
+
+public class Cli {
+	
+
+public class Cliente {
+	
+	Cliente cliente = new Cliente();
+	private String cli;
+	private String phone;
+	private String end;
+	private String ambiente;
+	private String pgto;
+	public Cliente getCliente() {
+		return cliente;
+	}
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+	public String getCli() {
+		return cli;
+	}
+	public void setCli(String cli) {
+		this.cli = cli;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getEnd() {
+		return end;
+	}
+	public void setEnd(String end) {
+		this.end = end;
+	}
+	public String getAmbiente() {
+		return ambiente;
+	}
+	public void setAmbiente(String ambiente) {
+		this.ambiente = ambiente;
+	}
+	public String getPgto() {
+		return pgto;
+	}
+	public void setPgto(String pgto) {
+		this.pgto = pgto;
+	}
+	
+	
+	
+	
+	
+	}	 
+}
+	
+
+
+
---
+++ src/Orcamento.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+
+		public class Orcamento {
+
+			public static void main(String[] args) {
+				Scanner sc = new Scanner(System.in);
+				Cli cli = new Cli();
+				
+				
+			}				
+
+}
